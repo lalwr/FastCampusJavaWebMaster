12e236a91e789b04f45a740feadbe4467f5b0bdf
lalwr <lalwrj@gmail.com>
2018-03-21 18:25:54 +0900

    백준 알고리즘 단어 정렬

---
+++ JavaAlgorithm_01/src/Algorithm_WordSort.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Algorithm_WordSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = Integer.parseInt(sc.nextLine());
+        String array[] = new String[count];
+
+        HashSet hs = new HashSet();
+        for(int i =0; i < count; i++){
+            hs.add(sc.nextLine());
+        }
+
+        int size = hs.size()
+        String[] wordArray = new String[size];
+        hs.toArray(wordArray);
+
+        Arrays.sort(wordArray, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+
+        for(int i=0; i<size; i++){
+            
+        }
+
+        Arrays.sort(wordArray);
+
+        for(int i =0; i < wordArray.length; i++){
+            System.out.println(wordArray[i]);
+        }
+
+    }
+}
