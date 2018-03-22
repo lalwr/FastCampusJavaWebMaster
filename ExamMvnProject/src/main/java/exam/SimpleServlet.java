7c4b7cd46e31fe023892a7f743a843aebc0ba2d9
lalwr <lalwrj@gmail.com>
2018-03-22 18:29:07 +0900

    Java Maven Servlet 프로젝트 서블릿 구현

---
+++ ExamMvnProject/src/main/java/exam/SimpleServlet.java
@@ -0,0 +1,29 @@
+package exam;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest reqest, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().println("Hello World!");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet " + this.getServletName() + " has started");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet " + this.getServletName() + " has stopped");
+    }
+
+}
+
