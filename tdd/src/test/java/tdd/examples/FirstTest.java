bb6cf248bf9bf094f441f025f3986e261bced25c
lalwr <lalwrj@gmail.com>
2018-03-28 11:44:47 +0900

    tdd 테스

---
+++ tdd/src/test/java/tdd/examples/FirstTest.java
@@ -0,0 +1,18 @@
+package tdd.examples;
+
+import org.junit.Test;
+
+public class FirstTest {
+    @Test
+    public void test1() throws Exception{
+        System.out.println("test1");
+    }
+    @Test
+    public void test2() throws Exception{
+        System.out.println("test2");
+    }
+    @Test
+    public void test3() throws Exception{
+        System.out.println("test3");
+    }
+}
