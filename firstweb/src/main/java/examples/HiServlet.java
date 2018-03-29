162fdcb2d48a2bdcf78f26af6a909ea44a5ff044
lalwr <lalwrj@gmail.com>
2018-03-29 15:43:46 +0900

    Httpsession구

--- firstweb/src/main/java/examples/HiServlet.java
+++ firstweb/src/main/java/examples/HiServlet.java
@@ -5,10 +5,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HiServlet", urlPatterns = "/hi2")
+@WebServlet(name = "HiServlet", urlPatterns = "/hi")
 public class HiServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -17,7 +18,16 @@ public class HiServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html"); //오타 나면 다운로드됨..
 
-        
+        HttpSession session = request.getSession();
+        System.out.println(session.getId());
+        if( session.isNew()){
+            session.setAttribute("name","kim");
+            System.out.println("session start");
+        }else{
+            System.out.println(session.getAttribute("name"));
+            session.getId();
+        }
+
 
 
         PrintWriter pw = response.getWriter();
