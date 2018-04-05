4d9aae9799dc869baa890c09ad5d5b3735059116
lalwr <lalwrj@gmail.com>
2018-04-05 17:24:55 +0900

    docs : 주석 추

--- config/mvcexam/src/main/java/kr/co/sunnyvale/mvcexam/controller/GoodsController.java
+++ config/mvcexam/src/main/java/kr/co/sunnyvale/mvcexam/controller/GoodsController.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @Controller
 public class GoodsController {
+
+	/*
+		http://localhost:8080/goods/{id} : id부분은 바뀔수 있다.
+		@PathVariable : path의 일부분을 읽어들일 때 사용
+		@RequestHeader : 요청정보중에서 헤더값을 읽을떄 사용, User-Agent헤더가 없으면 User-Agent변수에 기본값으로 "myBrowser"를 설정
+
+	*/
 	@GetMapping("/goods/{id}")
 	public String getGoodsById(@PathVariable(name="id") int id,
 							   @RequestHeader(value="User-Agent", defaultValue="myBrowser") String userAgent,
