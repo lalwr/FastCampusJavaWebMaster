e1b00fcc05aee9e0f7736ab7c545ab78adc1367b
lalwr <lalwrj@gmail.com>
2018-04-06 18:41:32 +0900

    docs : 주석 추가 및 sql파일 업로드

--- helloboard/src/main/java/examples/helloboard/controller/BoardController.java
+++ helloboard/src/main/java/examples/helloboard/controller/BoardController.java
@@ -47,6 +47,18 @@ public class BoardController {
         List ressultList = list.subList(startNo, endNo); //페이지에 보여줄 글 가져오기
 
         modelMap.addAttribute("list", ressultList);
+
+        //TODO
+        // searchType(title, content, titleAndContent), searchStr 이 있을 경우
+        // 1. 검색된 결과의 총 수
+        // 2. 한페이지에 보여줄 수
+        // 1,2를 이용하여 페이징 처리에 해당하는 값을 구할 수 있다. 만약 전체가 97페이지면?
+        // <<    <     1     2     3    4   5  >  >>    > 를 누르면 6페이지를 보여준다.
+        // <<    <     6     7     8    9   10  >  >>
+        // <<    <    96    97                  >  >>
+        // 3. page에 해당하는 검색 결과 목록
+        // 검색조건 값이 없을 경우 처리는?
+
         return "list";
     }
 
@@ -68,6 +80,12 @@ public class BoardController {
     public String boardDetail(@PathVariable(value ="no") int no, ModelMap modelMap) throws Exception{
         BoardVO boardVO = (BoardVO) list.get(no-1);
         modelMap.addAttribute("boardVO", boardVO);
+
+        // 1. no 해당하는 게시물을 한건 읽어온다.
+        // 2. no 이전 글, boardId의 이후 글. (검색어도 신경써라!!!)
+        //    이전글 : 100보다 작으면서 가장 큰 boardId
+        //    이후글 : 100보다 크면서 가장 작은 boardId
+
         return "detail";
     }
 
